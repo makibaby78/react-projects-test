61988406917edaeb7986c69354de76877b8f2414
Macky Baby <raindrops.dark@gmail.com>
2022-04-18 17:58:24 +0800

    Updates

---
+++ precache-manifest.bd9c394242ff039152c2761083675ffa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a33164fe973cb7c03cb1",
+    "url": "/react-projects-test/static/css/main.e49508b5.chunk.css"
+  },
+  {
+    "revision": "a33164fe973cb7c03cb1",
+    "url": "/react-projects-test/static/js/main.a33164fe.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/react-projects-test/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "5606a65092c4aeeee083",
+    "url": "/react-projects-test/static/js/2.5606a650.chunk.js"
+  },
+  {
+    "revision": "b3a6dad211cd49f34b56",
+    "url": "/react-projects-test/static/js/runtime~main.b3a6dad2.js"
+  },
+  {
+    "revision": "a9daebab7dab1d6be8e336a10269e491",
+    "url": "/react-projects-test/static/media/Sample Logo 1.a9daebab.png"
+  },
+  {
+    "revision": "57a8d90fbe52cdc657be8d679f925066",
+    "url": "/react-projects-test/static/media/Montserrat-Light.57a8d90f.ttf"
+  },
+  {
+    "revision": "6890001898a6fc1b36ddc2ed0195e2d8",
+    "url": "/react-projects-test/static/media/Montserrat-Regular.68900018.ttf"
+  },
+  {
+    "revision": "718651c5afbbca8153f7c78027f16a85",
+    "url": "/react-projects-test/static/media/TrendSansOne.718651c5.ttf"
+  },
+  {
+    "revision": "0c12ee2fd868459a1db633259b2ba50a",
+    "url": "/react-projects-test/static/media/Montserrat-Bold.0c12ee2f.ttf"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "/react-projects-test/static/media/BebasNeue-Regular.47f711bd.ttf"
+  },
+  {
+    "revision": "aabdebbda12fae03bd49b4e472146d46",
+    "url": "/react-projects-test/index.html"
+  }
+];
