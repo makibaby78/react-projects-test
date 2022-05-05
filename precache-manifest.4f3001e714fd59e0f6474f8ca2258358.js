b47c9f589791981ce08b9e8be76232ebc3980647
Macky Baby <raindrops.dark@gmail.com>
2022-05-05 21:58:34 +0800

    Updates

---
+++ precache-manifest.4f3001e714fd59e0f6474f8ca2258358.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "313f7f270efa203d118a",
+    "url": "/react-projects-test/static/css/main.0e1cf195.chunk.css"
+  },
+  {
+    "revision": "313f7f270efa203d118a",
+    "url": "/react-projects-test/static/js/main.313f7f27.chunk.js"
+  },
+  {
+    "revision": "ce47ab81b35290e2776b",
+    "url": "/react-projects-test/static/js/1.ce47ab81.chunk.js"
+  },
+  {
+    "revision": "ab4a7973d06f5686791b",
+    "url": "/react-projects-test/static/js/2.ab4a7973.chunk.js"
+  },
+  {
+    "revision": "fe660b354ee489abc6ff",
+    "url": "/react-projects-test/static/js/runtime~main.fe660b35.js"
+  },
+  {
+    "revision": "a9daebab7dab1d6be8e336a10269e491",
+    "url": "/react-projects-test/static/media/Sample Logo 1.a9daebab.png"
+  },
+  {
+    "revision": "f1c3651a3da5e12a672e3b95ec6da5a1",
+    "url": "/react-projects-test/static/media/Cupcakes Brown lq.f1c3651a.jpg"
+  },
+  {
+    "revision": "62b09ae897c83836ba5361f1788fb763",
+    "url": "/react-projects-test/static/media/Cupcakes lq.62b09ae8.jpg"
+  },
+  {
+    "revision": "e49b5c638c928f0d4ca230737982c583",
+    "url": "/react-projects-test/static/media/Cake lq.e49b5c63.jpg"
+  },
+  {
+    "revision": "55bf274eeeb04dd4f249d2cd997dd0fd",
+    "url": "/react-projects-test/static/media/Generic Muffins.55bf274e.jpg"
+  },
+  {
+    "revision": "d7eee0c2e62d0af675db76c38affffbd",
+    "url": "/react-projects-test/static/media/Brown Cake.d7eee0c2.jpg"
+  },
+  {
+    "revision": "d3f90ee7760844d22f4cba50f3d05c43",
+    "url": "/react-projects-test/static/media/Cookies.d3f90ee7.jpg"
+  },
+  {
+    "revision": "55fc7d16f37c66646220afbfae27fe58",
+    "url": "/react-projects-test/static/media/pexels-lucie-liz-3256807.55fc7d16.jpg"
+  },
+  {
+    "revision": "69ece5c85a2692b5178173682156af1f",
+    "url": "/react-projects-test/static/media/hazelnut-cupcake.69ece5c8.png"
+  },
+  {
+    "revision": "6557acaeb8f8f89a8f512db72c1435f2",
+    "url": "/react-projects-test/static/media/glaze-donut.6557acae.png"
+  },
+  {
+    "revision": "447f971986fdf76c088eb4e58fee70c4",
+    "url": "/react-projects-test/static/media/Donut.447f9719.png"
+  },
+  {
+    "revision": "b06a8293c0349e57915cbb861f443638",
+    "url": "/react-projects-test/static/media/croissants.b06a8293.png"
+  },
+  {
+    "revision": "1fcb36758564f8b2ff9f6d3d2013fad7",
+    "url": "/react-projects-test/static/media/choco-donut.1fcb3675.png"
+  },
+  {
+    "revision": "d92798bbce16760d44378242bf029532",
+    "url": "/react-projects-test/static/media/native-bread.d92798bb.png"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "/react-projects-test/static/media/BebasNeue-Regular.47f711bd.ttf"
+  },
+  {
+    "revision": "57a8d90fbe52cdc657be8d679f925066",
+    "url": "/react-projects-test/static/media/Montserrat-Light.57a8d90f.ttf"
+  },
+  {
+    "revision": "6890001898a6fc1b36ddc2ed0195e2d8",
+    "url": "/react-projects-test/static/media/Montserrat-Regular.68900018.ttf"
+  },
+  {
+    "revision": "718651c5afbbca8153f7c78027f16a85",
+    "url": "/react-projects-test/static/media/TrendSansOne.718651c5.ttf"
+  },
+  {
+    "revision": "0c12ee2fd868459a1db633259b2ba50a",
+    "url": "/react-projects-test/static/media/Montserrat-Bold.0c12ee2f.ttf"
+  },
+  {
+    "revision": "2192443373f4e6d6fe159bd11d497c24",
+    "url": "/react-projects-test/static/media/ThirstyScriptExtraBold.21924433.otf"
+  },
+  {
+    "revision": "c513283c055aa42238561a31ad9e6737",
+    "url": "/react-projects-test/static/media/ThirstyRoughLight.c513283c.otf"
+  },
+  {
+    "revision": "d6ee8f76acb15b46f0b3ceda33e676ee",
+    "url": "/react-projects-test/static/media/ThirstyRoughRegular.d6ee8f76.otf"
+  },
+  {
+    "revision": "8588f6fedb41275ed6501522a7ad8f2a",
+    "url": "/react-projects-test/static/media/MilasianCircaMediumPERSONAL.8588f6fe.ttf"
+  },
+  {
+    "revision": "2fe5fb7f37535452bc6a61d52d61660a",
+    "url": "/react-projects-test/static/media/libre-baskerville.bold.2fe5fb7f.ttf"
+  },
+  {
+    "revision": "5f88bc53fb9ce8be3a3d6b497ec68f7f",
+    "url": "/react-projects-test/index.html"
+  }
+];
